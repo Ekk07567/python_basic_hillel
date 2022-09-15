304d8d6205581f48aec6d8823e0b2ce4a59cdc71
Ekk07567 <109170567+Ekk07567@users.noreply.github.com>
2022-09-15 18:29:05 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+A = int(input("Сторона A:"))
+h = int(input("Выстоа h:"))
+S = (A * h)/2
+print(S)
+git remote add origin https://github.com/Ekk07567/python_basic_hillel.git
+git branch -M main
+git push -u origin main
